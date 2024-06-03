fabbcb1b638ae2d770c529139b148e16baa0f5b3
Max K <66796793+maxsrobotics@users.noreply.github.com>
2024-06-03 07:02:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ import os
 import ast
 import sys
 
-# Settings:
+# Settings - THIS WILL CHANGE PER YOUR CONFIGURATION:
 ####################################################################
-os.environ["DIGIKEY_CLIENT_ID"] = "QlnQBYLGQCUJPG2xAydT5zUbrSzsARya"
-os.environ["DIGIKEY_CLIENT_SECRET"] = "pCN17uORphL5KIys"
-os.environ["DIGIKEY_STORAGE_PATH"] = "cache_dir"
-schematic_file_path = "demo/demo.kicad_sch"
+os.environ["DIGIKEY_CLIENT_ID"] = "your-client-id-here" # <- Register with DigiKey API
+os.environ["DIGIKEY_CLIENT_SECRET"] = "your-client-secret-here" # <- ^
+os.environ["DIGIKEY_STORAGE_PATH"] = "cache_dir" # Needed by DigiKey API
+schematic_file_path = "demo/demo.kicad_sch" # Path to the schematic
 ####################################################################
 
+# Get the component value field from KiCAD
 def get_component_value(schematic_file, ref_des):
     try:
         with open(schematic_file, "r") as file:
             data = file.read() 
 
+        # RegEx to pick out the value
         component_pattern = re.compile(
             r"\(symbol\n"
             r'\s+\(lib_id ".*?"\)\n'
@@ -35,6 +37,7 @@ def get_component_value(schematic_file, ref_des):
             re.MULTILINE | re.DOTALL,
         )
 
+        # If found, return the value
         match = component_pattern.search(data)
         if match:
             return match.group(1)
@@ -49,11 +52,13 @@ def get_component_value(schematic_file, ref_des):
         return None
 
 
+# Set the value field of the component in KiCAD
 def set_component_value(schematic_file, ref_des, new_value):
     try:
         with open(schematic_file, "r") as file:
             data = file.read()
 
+        # More RegEx to specify where to set the field
         component_pattern = re.compile(
             r"(\(symbol\n"
             r'\s+\(lib_id ".*?"\)\n'
@@ -69,6 +74,7 @@ def set_component_value(schematic_file, ref_des, new_value):
             re.MULTILINE | re.DOTALL,
         )
 
+        # If field found, set it!
         match = component_pattern.search(data)
         if match:
             new_data = data[: match.start(2)] + new_value + data[match.end(2) :]
@@ -86,16 +92,19 @@ def set_component_value(schematic_file, ref_des, new_value):
         return False
 
 
+# Get the reference designator from the command line
 component_ref = str(sys.argv[1])
 search = str(get_component_value(schematic_file_path, component_ref))
 
 search_requests = KeywordSearchRequest(keywords=search, record_count=10)
 results = digikey.keyword_search(body=search_requests)
 
+# The JSON coming out of it is using single quotes, not the standard double quotes. Therefore, we use literal_eval()
 parsed = ast.literal_eval(str(results))
 
 outputDict = {}
 
+# Create a nice, easy to parse dictionary
 for i in range(len(parsed['products'])):
    
     # Create a new dictionary for each product
@@ -105,17 +114,20 @@ for i in range(len(parsed['products'])):
         'part_num': str(parsed['products'][i]['manufacturer_part_number'])
     }
     
-    # Append the duct dictionary to the output dictionary
+    # Append the product dictionary to the output dictionary
     outputDict[i] = product_dict
 
+# Filter out quantities greater than 10. In the future, we plan to accept the quantity as a command-line argument.
 specified_quantity = 10
 outputDict = {k: v for k, v in outputDict.items() if int(v['quantity']) <= specified_quantity}
 
+# Print it all out!
 print("Possible Parts:\n")
 for key, value in outputDict.items():
     print(f"{key}. Part Number: {value['part_num']}, Unit Price: ${value['unit_price']}, Quantity: {value['quantity']}")
 
 print(f"Best Product Based on Price: {outputDict[next(iter(outputDict))]['part_num']}, at ${outputDict[next(iter(outputDict))]['unit_price']}")
 print("Assigning Value to Schematic Symbol Now...")
+# Set the schematic value to append the part number
 set_component_value(schematic_file_path, component_ref, str({outputDict[next(iter(outputDict))]['part_num']}).strip("{''}") + " - " + str(search))
 print("Done!")
